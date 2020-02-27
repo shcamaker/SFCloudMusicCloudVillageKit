663f8fe0a2d884e00ddcfb68e542b2a7e4e1981f
Alex.Shen <alex.shen@wesoft.com>
2020-02-27 15:35:34 +0800

    kkk

---
+++ Example/Pods/Target Support Files/Pods-SFCloudMusicCloudVillageKit_Example/Pods-SFCloudMusicCloudVillageKit_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SFCloudMusicCloudVillageKit_Example : NSObject
+@end
+@implementation PodsDummy_Pods_SFCloudMusicCloudVillageKit_Example
+@end
